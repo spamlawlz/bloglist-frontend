217e6d8091bc5ebe7fb109e1eeca68d194002254
Sam Lawler <sam.lawler@gmail.com>
2024-02-22 14:22:01 -0800

    5.7 Blog List Frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,59 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+    const [showDetails, setShowDetails] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const addLike = () => {
+        console.log('add a like!')
+    }
+
+    const blogDetails = () => (
+            <div>
+                <div>
+                <a href={blog.url}>{blog.url}</a>
+                </div>
+                <div>
+                likes {blog.likes} <button onClick={addLike}>like</button>
+                </div>
+                <div>
+                {blog.user ?
+                    blog.user.name :
+                    null
+                }
+                </div>
+            </div>
+    )
+
+    const detailsButton = () => {
+        if (showDetails) {
+            return (
+                <button onClick={() => setShowDetails(false)}>hide</button>
+            )
+        } else {
+            return (
+                <button onClick={() => setShowDetails(true)}>view</button>
+            )
+        }
+    }
+
+    return (
+        <div style={blogStyle}>
+            <div>
+            {blog.title} | {blog.author} {detailsButton()}
+            </div>
+            {showDetails ?
+                blogDetails() :
+                null}
+        </div>  
+    )
+}
 
 export default Blog
